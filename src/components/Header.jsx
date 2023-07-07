15fdb1f35d57e55dbc4792c424b652b2da406b04
whyeram <eram2106485@st.jmi.ac.in>
2023-07-07 22:11:07 +0530

    First commit to Keeper app

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaPen } from 'react-icons/fa';
+
+function Header() {
+  return (
+    <header>
+      <h1><FaPen size={24} /> Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
